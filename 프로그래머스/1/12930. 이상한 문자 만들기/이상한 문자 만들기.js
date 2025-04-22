b5aac1942e82a1807b9e6473c1ac59e91aed52e1
minji105 <116670097+minji105@users.noreply.github.com>
2025-04-23 01:51:56 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,7 +1,7 @@
 function solution(s) {
-    return s.split(' ').map(v => v = char(v)).join(' ');
-}
-
-function char(word) {
-    return word.split('').map((v, i) => v = i % 2 ? v.toLowerCase() : v.toUpperCase()).join('');
+    return s.split(' ')
+            .map(word => 
+                 [...word].map((v, i) => i % 2 ? v.toLowerCase() : v.toUpperCase()).join('')
+                )
+            .join(' ');
 }
