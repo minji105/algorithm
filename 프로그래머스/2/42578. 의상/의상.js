af09fde07cc18955e9d129074cfa7d1174a99724
minji105 <116670097+minji105@users.noreply.github.com>
2025-04-23 10:11:29 +0900

    [level 2] Title: 의상, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,8 +1,8 @@
 function solution(input) {
     let clothes = {};
     
-    for(let i = 0; i<input.length; i++) {
-        clothes[input[i][1]] = clothes[input[i][1]] ? clothes[input[i][1]] + 1 : 1;
+    for (let i of input) {
+        clothes[i[1]] = clothes[i[1]] ? clothes[i[1]] + 1 : 1;
     }
 
     return Object.values(clothes).reduce((a, c) => (c + 1) * a, 1) - 1;
