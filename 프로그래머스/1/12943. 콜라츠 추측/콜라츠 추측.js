c00d8bc3505c9f6f7de9533d1a1ede1e6f764ded
minji105 <116670097+minji105@users.noreply.github.com>
2025-04-23 13:20:44 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,16 +1,8 @@
 function solution(num) {
     var answer = 0;
-    while (num !== 1) {
-        if (num % 2) {
-            num *= 3;
-            num += 1;
-        } else num /= 2;
-        
+    while (num !== 1 && answer !== 500) {
+        num % 2 ? num = num * 3 + 1 : num /= 2;
         answer++;
-        if (answer === 500) { 
-            answer = -1; 
-            break; 
-        }
     }
-    return answer;
+    return num === 1 ? answer : -1;
 }
