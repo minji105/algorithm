13f3fd6986448770cd015c6d4f5cde834d981f3d
minji105 <ajhjmj9@naver.com>
2025-04-24 14:01:57 +0900

    Revert "[level 1] Title: 콜라츠 추측, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub"
    This reverts commit c00d8bc3505c9f6f7de9533d1a1ede1e6f764ded.

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,8 +1,16 @@
 function solution(num) {
     var answer = 0;
-    while (num !== 1 && answer !== 500) {
-        num % 2 ? num = num * 3 + 1 : num /= 2;
+    while (num !== 1) {
+        if (num % 2) {
+            num *= 3;
+            num += 1;
+        } else num /= 2;
+        
         answer++;
+        if (answer === 500) { 
+            answer = -1; 
+            break; 
+        }
     }
-    return num === 1 ? answer : -1;
+    return answer;
 }
