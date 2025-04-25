6a17d47c0be8b1eb3fd39ee17ab0b6d055e436db
minji105 <116670097+minji105@users.noreply.github.com>
2025-04-25 23:32:57 +0900

    [Bronze II] Title: 수 정렬하기, Time: 100 ms, Memory: 9708 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -1,6 +1,4 @@
 const fs = require("fs");
 const n = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-n.shift();
-const answer = n.map(v => v / 1).sort((a, b) => a - b);
-answer.forEach(v => console.log(v))
+console.log(n.slice(1).map(v => v / 1).sort((a, b) => a - b).join('\n'));
