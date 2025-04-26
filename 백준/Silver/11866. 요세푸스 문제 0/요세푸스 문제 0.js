c139f94647a4a01d204c38c2191a5414a3fe7e3b
minji105 <116670097+minji105@users.noreply.github.com>
2025-04-26 14:56:00 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 92 ms, Memory: 9764 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.js
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.js
@@ -5,16 +5,12 @@ const [N, K] = n.split(' ').map(Number);
 let q = new Array(N).fill(1).map((x, i) => x + i);
 
 let result = [];
-let order = 1;
+let index = K - 1;
 
 while (q.length > 0) {
-  if (order !== K) {
-    q.push(q.shift());
-    order++;
-  }
-  else {
-    result.push(q.shift());
-    order = 1;
-  }
+  index %= q.length;
+  result.push(q.splice(index, 1))
+
+  index += (K - 1)
 }
 console.log('<' + result.join(', ') + '>');
