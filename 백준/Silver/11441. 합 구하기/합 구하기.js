7f85e803d5a9101815a59359747cc18b81b5d68b
minji105 <116670097+minji105@users.noreply.github.com>
2025-04-27 14:03:06 +0900

    [Silver III] Title: 합 구하기, Time: 332 ms, Memory: 44420 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.js
+++ 백준/Silver/11441. 합 구하기/합 구하기.js
@@ -2,11 +2,16 @@ const fs = require("fs");
 const n = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 let nums = n[1].split(' ').map(Number);
-nums.forEach((v, i) => nums[i] = v += nums[i - 1] ? nums[i - 1] : 0)
+for (let i = 1; i < n[0] / 1; i++)
+  nums[i] += nums[i - 1];
 
-let M = Number(n[2]);
+const M = Number(n[2]);
 let index = 3;
-while (M-- > 0) {
-  const [start, end] = n[index++].split(' ').map(v => v / 1 - 1);
-  console.log(start === 0 ? nums[end] : nums[end] - nums[start - 1]);
+const result = [];
+
+for (let i = 0; i < M; i++) {
+  const [start, end] = n[index++].split(' ').map(Number);
+  result.push(start === 1 ? nums[end - 1] : nums[end - 1] - nums[start - 2]);
 }
+
+console.log(result.join('\n'));
