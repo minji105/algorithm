bca82ca77b25253e331f10b86cd42caab3c47397
minji105 <116670097+minji105@users.noreply.github.com>
2025-04-29 14:42:24 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 524 ms, Memory: 85112 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const n = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [N, M] = n[0].split(' ').map(Number);
+
+const prefix = new Array(N + 1).fill(0).map(v => v = new Array(N + 1).fill(0));
+
+for (let i = 1; i <= N; i++) {
+  const line = n[i].split(' ').map(Number);
+
+  for (let j = 1; j <= N; j++) {
+    prefix[i][j] = line[j - 1] + prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1];
+  }
+}
+
+const result = [];
+
+for (let i = 0; i < M; i++) {
+  const [x1, y1, x2, y2] = n[N + i + 1].split(' ').map(Number);
+  result.push(prefix[x2][y2] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1]);
+}
+
+console.log(result.join('\n'));
