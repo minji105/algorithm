146af994b82dfc0816a178fa45f1357c10952dd0
minji105 <116670097+minji105@users.noreply.github.com>
2025-05-05 19:13:11 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 5.39 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -7,11 +7,13 @@ function solution(bridge_length, weight, truck_weights) {
     while (truck_weights.length > 0 || passing_weight > 0) {
         sec++;
         
+        // 맨 앞 트럭 진입한 지 bridge_length초 지나면 pop
         if (head < queue.length && sec - queue[head].entry === bridge_length) { 
             passing_weight -= queue[head].weight;
             head++;
         }
         
+        // 다음 트럭 올라갈 수 있으면 push. 진입시점 = 현재시간
         if (truck_weights.length > 0 && truck_weights[0] + passing_weight <= weight) {
             const truck = truck_weights.shift()
             queue.push({ weight: truck, entry: sec });
