d603c6173231d8f8a52b379089021d92c14614b8
minji105 <116670097+minji105@users.noreply.github.com>
2025-06-06 19:01:34 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.08 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,26 +1,14 @@
 function solution(n) {
-    let binArr = ('0' + n.toString(2)).split('').reverse();
-    const one = binArr.filter(v => v === '1').length;
-    
-    while (1) {
-        let index = 0;
-        
-        while (1) {
-            if (binArr[index] === '0') {
-                binArr[index] = '1';
-                break;
-            } else {
-                binArr[index] = '0';
-                index++;
-            }
-            if (index === binArr.length) {
-                binArr.push('1');
-                break;
-            }
-        }
-        
-        if (binArr.filter(v => v === '1').length === one) break;
+    const countOne = (num) => {
+        return num.toString(2).split('1').length - 1;
     }
     
-    return parseInt(binArr.reverse().join(''), 2);
+    const one = countOne(n);
+    let result = n + 1;
+    
+    while (countOne(result) !== one) {
+        result++;
+    }
+    
+    return result;
 }
