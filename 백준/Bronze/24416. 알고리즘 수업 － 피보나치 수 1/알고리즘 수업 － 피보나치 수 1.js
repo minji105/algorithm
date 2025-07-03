e6023446691cf5b99d71951f3a861f1f809a7b6a
minji105 <116670097+minji105@users.noreply.github.com>
2025-07-03 10:35:57 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 1256 ms, Memory: 9960 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.js
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 let N = fs.readFileSync("/dev/stdin").toString() / 1;
 
 let countRec = 0;
-let countDP = 0;
 
 const fibRec = (n) => {
   if (n === 1 || n === 2) {
@@ -12,16 +11,6 @@ const fibRec = (n) => {
   return fibRec(n - 1) + fibRec(n - 2);
 };
 
-const fibDP = (n) => {
-  const fib = [0, 1, 1];
-  for (let i = 3; i <= n; i++) {
-    countDP++;
-    fib[i] = fib[i - 1] + fib[i - 2];
-  }
-  return fib[n];
-};
-
 fibRec(N);
-fibDP(N);
 
-console.log(countRec, countDP);
+console.log(countRec, N - 2);
