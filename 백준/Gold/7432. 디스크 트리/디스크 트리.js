e2ba2ef4938b661b55c845b482607c55bcb5c309
minji105 <116670097+minji105@users.noreply.github.com>
2025-07-10 13:25:17 +0900

    [Gold III] Title: 디스크 트리, Time: 888 ms, Memory: 21516 KB -BaekjoonHub

---
+++ 백준/Gold/7432. 디스크 트리/디스크 트리.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+
+let tree = {};
+
+for (let i = 1; i <= input[0]; i++) {
+  const path = input[i].split('\\');
+  let current = tree;
+
+  for (const dir of path) {
+    if (!current[dir]) {
+      current[dir] = {};  // current.dir = {}
+    }
+    current = current[dir]; // current -> current.dir
+  }
+}
+
+const printTree = (node, depth) => {
+  const keys = Object.keys(node).sort();
+  for (const key of keys) {
+    console.log(' '.repeat(depth) + key);
+    printTree(node[key], depth + 1);
+  }
+};
+
+printTree(tree, 0);
