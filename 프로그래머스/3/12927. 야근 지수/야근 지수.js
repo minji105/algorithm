507eb3704d3352eae6993b567bf13117d0143440
minji105 <116670097+minji105@users.noreply.github.com>
2025-07-16 11:36:08 +0900

    [level 3] Title: 야근 지수, Time: 162.74 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.js
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -1,17 +1,70 @@
-function solution(n, works) {
-    works = works.sort((a, b) => b - a);
-    
-    while (n-- > 0) {
-        works[0]--;
-        
-        for (let i = 0; i < works.length; i++) {
-            if (works[i] < works[i + 1]) {
-                [works[i], works[i + 1]] = [works[i + 1], works[i]];
-            } else break;
-        }
-        
-        if (works[0] <= 0) break;
+class MaxHeap {
+    constructor() {
+        this.heap = [];
     }
     
-    return works.reduce((a, c) => a + c ** 2, 0);
+    push(v) {
+        this.heap.push(v);
+        
+        let i = this.heap.length - 1;
+        
+        while (i > 0) {
+            let parent = Math.floor((i - 1) / 2);
+            if (this.heap[parent] >= this.heap[i]) break;
+            [this.heap[parent], this.heap[i]] = [this.heap[i], this.heap[parent]];
+            i = parent;
+        }
+    }
+    
+    pop() {
+        const max = this.heap[0];
+        
+        if (this.heap.length > 0) {
+            this.heap[0] = this.heap.pop();
+            
+            let i = 0;
+            let len = this.heap.length;
+            
+            while (1) {
+                let left = i * 2 + 1;
+                let right = i * 2 + 2;
+                let max = i;
+                
+                if (left < len && this.heap[left] > this.heap[max]) {
+                    max = left;
+                }
+                if (right < len && this.heap[right] > this.heap[max]) {
+                    max = right;
+                }
+                
+                if (max === i) break;
+                [this.heap[max], this.heap[i]] = [this.heap[i], this.heap[max]];
+                i = max;
+            }
+        }
+        
+        return max;
+    }
+    
+    isEmpty() {
+        return this.heap.length === 0;
+    }
+}
+
+function solution(n, works) {
+    const heap = new MaxHeap();
+    
+    for (const work of works) {
+        heap.push(work);
+    }
+    
+    while (n-- > 0 && !heap.isEmpty()) {
+        let max = heap.pop();
+        
+        if (max > 0) {
+            heap.push(--max);
+        }
+    }
+    
+    return heap.heap.reduce((a, c) => a + c ** 2, 0);
 }
