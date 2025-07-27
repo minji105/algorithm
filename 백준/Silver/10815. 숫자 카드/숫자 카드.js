4e0e17f1d6e2befeffe0e2544e3c2887b64c9acf
minji105 <116670097+minji105@users.noreply.github.com>
2025-07-27 17:08:14 +0900

    [Silver V] Title: 숫자 카드, Time: 716 ms, Memory: 175320 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.js
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.js
@@ -1,30 +1,14 @@
 const fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
 
-const arrN = input[1].split(' ').sort((a, b) => a - b).map(Number);
-const arrM = input[3].split(' ').map(Number);
-
-const bn = (arr, num) => {
-  let pl = 0;
-  let pr = arr.length - 1;
-
-  while (pl <= pr) {
-    const mid = Math.floor((pl + pr) / 2);
-
-    if (num < arr[mid]) {
-      pr = mid - 1;
-    } else if (num > arr[mid]) {
-      pl = mid + 1;
-    } else return 1;
-  }
-
-  return 0;
-};
+const setN = new Set(input[1].split(' ').map(Number));
+const setM = new Set(input[3].split(' ').map(Number));
 
 const result = [];
 
-for (const n of arrM) {
-  result.push(bn(arrN, n));
+for (const num of setM) {
+  if (setN.has(num)) result.push(1);
+  else result.push(0);
 }
 
 console.log(result.join(' '));
